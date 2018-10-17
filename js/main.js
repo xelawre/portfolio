f78ff1443054f6d3e4a0461c915d0cf5f913801c
xelawre <axel.wredlert@student.kyh.se>
2018-10-17 17:33:07 +0200

    new flex layout in portfolio section

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
-function slideInText(event) {
-  event.preventDefault();
-  switch (event.target.classList[0]) {
-    case 'p-1':
-      document.getElementsByClassName('p-description')[0].classList.add('show-text');
-      break;
-    case 'p-2':
-      document.getElementsByClassName('p-description')[1].classList.add('show-text');
-      break;
-    case 'p-3':
-      document.getElementsByClassName('p-description')[2].classList.add('show-text');
-      break;
-    default:
-  }
-}
-
-function hideText(event) {
-  event.preventDefault();
-  switch (event.target.classList[0]) {
-    case 'p-1':
-      document.getElementsByClassName('p-description')[0].classList.remove('show-text');
-      break;
-    case 'p-2':
-      document.getElementsByClassName('p-description')[1].classList.remove('show-text');
-      break;
-    case 'p-3':
-      document.getElementsByClassName('p-description')[2].classList.remove('show-text');
-      break;
-    default:
-  }
-}
+// function slideInText(event) {
+//   event.preventDefault();
+//   switch (event.target.classList[0]) {
+//     case 'p-1':
+//       document.getElementsByClassName('p-description')[0].classList.add('show-text');
+//       break;
+//     case 'p-2':
+//       document.getElementsByClassName('p-description')[1].classList.add('show-text');
+//       break;
+//     case 'p-3':
+//       document.getElementsByClassName('p-description')[2].classList.add('show-text');
+//       break;
+//     default:
+//   }
+// }
+//
+// function hideText(event) {
+//   event.preventDefault();
+//   switch (event.target.classList[0]) {
+//     case 'p-1':
+//       document.getElementsByClassName('p-description')[0].classList.remove('show-text');
+//       break;
+//     case 'p-2':
+//       document.getElementsByClassName('p-description')[1].classList.remove('show-text');
+//       break;
+//     case 'p-3':
+//       document.getElementsByClassName('p-description')[2].classList.remove('show-text');
+//       break;
+//     default:
+//   }
+// }
